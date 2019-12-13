8f1b8542c310824194765fb79524c7e21cb7da2a
Galina <galina@Olegs-MacBook-Pro.local>
2019-12-13 16:15:45 +0300

    Workshop state component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: "Galina",
+//       age: "33"
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.name}</h1>
+//         <h3>
+//           <font color="#3AC1EF"> {this.state.age} years old</font>
+//         </h3>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: true 
+    }
+  }
+
+  render() {
+    let status
+
+    if (this.state.isLoggedIn) {
+      status = "You are currently logged in"
+    } else {
+      status = "You are currently logged out"
+    }
+
+
+    return (
+      <div>
+        <h1>{status}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
